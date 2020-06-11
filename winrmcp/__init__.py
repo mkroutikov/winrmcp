765a09ae19420bc3b9666e826f33ea63a5b26d23
Mike Kroutikov <mkroutikov@innodata.com>
2020-06-10 20:51:01 -0400

    version bump for auto-deploy

--- winrmcp/__init__.py
+++ winrmcp/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __description__ = 'Package to execute commads on remote Windows, do file copy to the remote machine'
 __author__ = 'Mike Kroutikov'
 __author_email__ = 'mkroutikov@innodata.com'
